1970497c39fadfaa0ad6566ce1e59418bf3190bd
Tina <x123719@gmail.com>
2025-01-18 21:58:55 +0800

    chore: add new note for hooks-api

--- src/hooks-api/useCurrentWeather.ts
+++ src/hooks-api/useCurrentWeather.ts
@@ -1,8 +1,8 @@
 import useSWR from 'swr'
-import { WEATHER_API_URL } from '../constants/baseUrls'
-import { weatherSWRFetcher } from '../utils/weatherSWRFetcher'
-import { type CurrentWeather } from '../types/CurrentWeather'
-import { urlWithQueryParams } from '../utils/urlWithQueryParams'
+import { WEATHER_API_URL } from '../constants/baseUrls' // 定義的API URL
+import { weatherSWRFetcher } from '../utils/weatherSWRFetcher'// 自定義的fetcher
+import { type CurrentWeather } from '../types/CurrentWeather'// 定義的型別
+import { urlWithQueryParams } from '../utils/urlWithQueryParams'// 自定義的fetcher
 
 interface CurrentWeatherProps {
   lat?: number,
